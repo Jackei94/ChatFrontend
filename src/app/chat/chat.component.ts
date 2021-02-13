52b2712e927e02805079dae5c86a882eccaa83b1
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2021-02-13 12:08:34 +0100

    Init

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,65 @@
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {ChatService} from './shared/chat.service';
+import {Observable, Subject, Subscription} from 'rxjs';
+import {take, takeUntil} from 'rxjs/operators';
+import {ChatClient} from './shared/chat-client.model';
+import {ChatMessage} from './shared/chat-message.model';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  message = new FormControl('');
+  nickNameFc = new FormControl('');
+  date = new FormControl('');
+  messages: ChatMessage[] = [];
+  unsubscribe$ = new Subject();
+  clients$: Observable<ChatClient[]> | undefined;
+  chatClient: ChatClient | undefined;
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit(): void {
+    this.clients$ = this.chatService.listenForClients();
+    this.chatService.listenForMessages()
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(message => {
+        console.log('Hello');
+        this.messages.push(message);
+      });
+    this.chatService.listenForWelcome()
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(welcome => {
+        this.messages = welcome.messages;
+        this.chatClient = this.chatService.chatClient = welcome.client;
+      });
+    if (this.chatService.chatClient) {
+    this.chatService.sendNickName(this.chatService.chatClient.nickName);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  sendMessage(): void {
+    console.log(this.message.value);
+    this.chatService.sendMessage(this.message.value);
+    this.message.reset();
+  }
+  sendNickName(): void {
+    if (this.nickNameFc.value) {
+      this.chatService.sendNickName(this.nickNameFc.value);
+    }
+  }
+  sendDate(): void {
+    this.chatService.sendDate(this.date.value);
+  }
+}
---
+++ src/app/chat/shared/chat.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {Socket} from 'ngx-socket-io';
+import {Observable} from 'rxjs';
+import {ChatClient} from './chat-client.model';
+import {ChatMessage} from './chat-message.model';
+import {WelcomeDto} from './welcome.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  chatClient: ChatClient | undefined;
+  constructor(private socket: Socket) { }
+
+  sendMessage(msg: string): void {
+    this.socket.emit('message', msg);
+  }
+
+  listenForMessages(): Observable<ChatMessage> {
+    return this.socket
+      .fromEvent<ChatMessage>('newMessage');
+  }
+
+  listenForClients(): Observable<ChatClient[]> {
+    return this.socket
+      .fromEvent<ChatClient[]>('clients');
+  }
+
+  listenForWelcome(): Observable<WelcomeDto> {
+    return this.socket
+      .fromEvent<WelcomeDto>('welcome');
+  }
+
+  getAllMessages(): Observable<ChatMessage[]> {
+    return this.socket
+      .fromEvent<ChatMessage[]>('allMessages');
+  }
+  sendNickName(nickname: string): void {
+    this.socket.emit('nickname', nickname);
+  }
+  sendDate(date: Date): void {
+    this.socket.emit('date', date);
+  }
+  disconnect(): void {
+    this.socket.disconnect();
+  }
+  connect(): void {
+    this.socket.connect();
+  }
+}
